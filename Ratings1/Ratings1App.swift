3e5250ffd41efeceb21d1522c2fa074f78949ea0
Omkale1319 <omk_intern@pre-scient.com>
2024-08-27 13:13:51 +0530

    Initial Commit

---
+++ Ratings1/Ratings1App.swift
@@ -0,0 +1,17 @@
+//
+//  Ratings1App.swift
+//  Ratings1
+//
+//  Created by PTPL-MACM-01 on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Ratings1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
